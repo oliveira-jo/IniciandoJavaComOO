939406441b4d16b7e2cdc61def8a77a42ad9a93f
Jonathan Oliveira <oliveira.jo@hotmail.com>
2016-05-10 23:20:25 -0300

    correção de nomenclaturas

--- src/IniciandoJava/FirstProgram.java
+++ src/IniciandoJava/FirstProgram.java
@@ -1,14 +1,14 @@
 /*
 * Primeiro programa em java, um Hello work simples apenas para entender a linguagens e suas formas
 */
-package Cap2Java;
+package IniciandoJava;
 
 public class FirstProgram {
     
-//    public static void main(String[] args){
-//    
-//        System.out.print("Hello Work \nHere is my first Program \n");
-//        
-//    }
+    public static void main(String[] args){
+    
+        System.out.print("Hello Work \nHere is my first Program \n");
+        
+    }
     
 }
